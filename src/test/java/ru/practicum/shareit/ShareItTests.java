e375e64302115f179b575a0aee4952113f63dd91
EllaShimanskaya <shimanskayaella@yandex.ru>
2023-01-29 21:37:24 +0300

    project Sprint 15 – working with branch add-bookings ver 1.4

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,11 +2,13 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class ShareItTests {
 
 	@Test
+	@Profile("Test")
 	void contextLoads() {
 	}
 
